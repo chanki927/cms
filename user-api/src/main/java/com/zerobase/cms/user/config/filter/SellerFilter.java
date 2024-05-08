678f6059deda7b88ade2e0f173ae600ebd9642a2
chanki2 <chanki2@naver.com>
2024-05-08 21:55:11 +0900

    feat : [006-CUSTOMER_balance] 고객에 대한 잔액 조회 구현

---
+++ user-api/src/main/java/com/zerobase/cms/user/config/filter/SellerFilter.java
@@ -0,0 +1,34 @@
+package com.zerobase.cms.user.config.filter;
+
+import com.zerobase.cms.user.service.customer.CustomerService;
+import com.zerobase.domain.config.JwtAuthenticationProvider;
+import com.zerobase.domain.domain.common.UserVo;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/seller/*")
+@RequiredArgsConstructor
+public class CustomerFilter implements Filter {
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final SellerService sellerService;
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String token = req.getHeader("X-AUTH-TOKEN");
+        if (!jwtAuthenticationProvider.validateToken(token)) {
+            throw new ServletException("Invalid Access");
+        }
+        UserVo vo = jwtAuthenticationProvider.getUserVo(token);
+        customerService.findByIdAndEmail(vo.getId(), vo.getEmail()).orElseThrow(
+                () -> new ServletException("Invalid Access")
+        );
+        chain.doFilter(request, response);
+    }
+}
