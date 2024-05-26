fc628e20b50e2746f7b35b09f2abb72218c728f2
chanki2 <chanki2@naver.com>
2024-05-26 23:48:37 +0900

    feat : [014-SHOW_CART] 카트에 보기 - 가격 / 수량 / 아이템 변경에 따른 대응

--- order-api/src/main/java/com/zerobase/cms/order/application/CartApplication.java
+++ order-api/src/main/java/com/zerobase/cms/order/application/CartApplication.java
@@ -10,6 +10,9 @@ import com.zerobase.cms.order.service.ProductSearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -38,9 +41,112 @@ public class CartApplication {
         return cartService.addcart(customerId, form);
     }
 
+    // 1. 장바구니에 상품을 추가했다.
+    // 2. 상품의 가격이나 수량이 변동된다.
+
+    public Cart getCart(Long customerId) {
+        Cart cart = refreshCart(cartService.getCart(customerId));
+        Cart returnCart = new Cart();
+        returnCart.setCustomerId(customerId);
+        returnCart.setProducts(cart.getProducts());
+        returnCart.setMessages(cart.getMessages());
+        cart.setMessages(new ArrayList<>());
+        // 메시지 없는 것
+        cartService.putCart(customerId, cart);
+        return returnCart;
+
+        // 2. 메시지를 보고 난 다음에는, 이미 본 메시지는 스팸이 되기 때문에 제거한다.
+    }
+
+    public void clearCart(Long customerId) {
+        cartService.putCart(customerId, null);
+    }
+
+    private Cart refreshCart(Cart cart) {
+        // 1. 상품이나 상품의 아이템의 정보, 가격, 수량이 변경되었는지 체크하고 그에 맞는 알림을 제공해 준다.
+        // 2. 상품의 수량, 가격을 우리가 임의로 변경한다.
+
+        Map<Long, Product> productMap = productSearchService.getListByProductIds(
+                        cart.getProducts().stream().map(Cart.Product::getId).collect(Collectors.toList()))
+                .stream()
+                .collect(Collectors.toMap(Product::getId, product -> product));
+
+        for (int i = 0; i < cart.getProducts().size(); i++) {
+            Cart.Product cartProduct = cart.getProducts().get(i);
+            Product p = productMap.get(cartProduct.getId());
+
+            if (p == null) {
+                cart.getProducts().remove(cartProduct);
+                i--;
+                cart.addMessages(cartProduct.getName() + " 상품이 삭제되었습니다.");
+                continue;
+            }
+
+            Map<Long, ProductItem> productItemMap = p.getProductItems().stream()
+                    .collect(Collectors.toMap(ProductItem::getId, productItem -> productItem));
+
+            // 아이템 1, 2
+            List<String> tmpMessages = new ArrayList<>();
+
+            for (int j = 0; j < cartProduct.getItems().size(); j++) {
+                Cart.ProductItem cartProductItem = cartProduct.getItems().get(j);
+                ProductItem pi = productItemMap.get(cartProductItem.getId());
+
+                if (pi == null) {
+                    cartProduct.getItems().remove(cartProductItem);
+                    j--;
+                    tmpMessages.add(cartProductItem.getName() + " 옵션이 삭제되었습니다.");
+                    continue;
+                }
+
+                boolean isPriceChanged = false, isCountNotEnough = false;
+
+                if (!cartProductItem.getPrice().equals(pi.getPrice())) {
+                    isPriceChanged = true;
+                    cartProductItem.setPrice(pi.getPrice());
+                }
+
+                if (!cartProductItem.getCount().equals(pi.getCount())) {
+                    isCountNotEnough = true;
+                    cartProductItem.setCount(pi.getCount());
+                }
+
+                if (isPriceChanged && isCountNotEnough) {
+                    tmpMessages.add(cartProductItem.getName() + " 가격 변동, 수량이 부족하여 구매 가능한 최대치로 변경되었습니다.");
+                } else if (isPriceChanged) {
+                    tmpMessages.add(cartProductItem.getName() + " 가격이 변동되었습니다.");
+                } else if (isCountNotEnough) {
+                    tmpMessages.add(cartProductItem.getName() + " 수량이 부족하여 구매 가능한 최대치로 변경되었습니다.");
+                }
+            }
+
+            //
+            if (cartProduct.getItems().size() == 0) {
+                cart.getProducts().remove(cartProduct);
+                i--;
+                cart.addMessages(cartProduct.getName() + " 상품의 옵션이 모두 없어져 구매가 불가능합니다.");
+                continue;
+            } else if (tmpMessages.size() > 0) {
+                StringBuilder builder = new StringBuilder();
+                builder.append(cartProduct.getName() + " 상품의 변동 사항 : ");
+                for (String message : tmpMessages) {
+                    builder.append(message);
+                    builder.append(",");
+                }
+
+                cart.addMessages(builder.toString());
+            }
+        }
+
+        cartService.putCart(cart.getCustomerId(), cart);
+        return cart;
+    }
+
     private boolean addAble(Cart cart, Product product, AddProductCartForm form) {
+//        if (form.getId().equals(product.getId()))
         Cart.Product cartProduct = cart.getProducts().stream().filter(p -> p.getId().equals(form.getId()))
-                .findFirst().orElseThrow(() -> new CustomException(NOT_FOUND_PRODUCT));
+                .findFirst().orElse(Cart.Product.builder().id(product.getId())
+                        .items(Collections.emptyList()).build());
         Map<Long, Integer> cartItemCountMap = cartProduct.getItems().stream()
                 .collect(Collectors.toMap(Cart.ProductItem::getId, Cart.ProductItem::getCount));
         Map<Long, Integer> currentItemCountMap = product.getProductItems().stream()
@@ -48,6 +154,11 @@ public class CartApplication {
         return form.getItems().stream().noneMatch(
                 formItem -> {
                     Integer cartCount = cartItemCountMap.get(formItem.getId());
+
+                    if (cartCount == null) {
+                        cartCount = 0;
+                    }
+
                     Integer currentCount = currentItemCountMap.get(formItem.getId());
                     return formItem.getCount() + cartCount > currentCount;
                 });
